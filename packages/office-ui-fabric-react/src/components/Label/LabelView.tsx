e851338dd513cf2527631548463e8452bd9232be
Avital Baral <t-avba@microsoft.com>
2018-07-05 10:49:38 -0700

    codepen example file, used Button for codepen button instead of Form (all for label component)

--- packages/office-ui-fabric-react/src/components/Label/LabelView.tsx
+++ packages/office-ui-fabric-react/src/components/Label/LabelView.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { LabelBasicExample } from './examples/Label.Basic.Example';
+import { Button } from '../..';
 
-const jsContent = require('!raw-loader!office-ui-fabric-react/src/components/Label/examples/Label.Basic.Example.tsx') as string;
-// const htmlContent = `<script src="//unpkg.com/office-ui-fabric-react/dist/office-ui-fabric-react.min.js"></script>
-//   <div id=\'content\'></div>`;
+const jsContent = require('!raw-loader!office-ui-fabric-react/src/components/Label/codepen_examples/Label.Basic.CodepenExample.txt') as string;
+const htmlContent = `<script src="//unpkg.com/office-ui-fabric-react/dist/office-ui-fabric-react.min.js"></script>
+<div id=\'content\'></div>`;
 
 const headContent = `<script type="text/javascript" src="https://unpkg.com/react@16/umd/react.development.js"></script><script type="text/javascript" src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>`;
 
 const valueData = {
   title: 'New Fabric Pen!',
+  html: htmlContent,
   head: headContent,
   js: jsContent,
   js_pre_processor: 'typescript'
@@ -24,7 +26,7 @@ export const LabelView = () => (
     <LabelBasicExample />
     <form action="https://codepen.io/pen/define" method="POST" target="_blank">
       <input type="hidden" name="data" value={JSONstring} />
-      <input type="submit" value="Codepen example" />
+      <Button type="submit" text="Codepen example" />
     </form>
   </div>
 );
